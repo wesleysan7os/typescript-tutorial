dd0b00220631a831bae02df65ecc416bf51e3a73
Silva <wesley.santos.silva@accenture.com>
2021-09-05 22:02:33 -0300

    compiling typescript

---
+++ sandbox.js
@@ -0,0 +1,7 @@
+var caracter = 'wes';
+console.log(caracter);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const caracter = 'wes';
+
+console.log(caracter);
+
+const inputs = document.querySelectorAll('input');
+
+console.log(inputs);
+
+inputs.forEach(input => {
+  console.log(input);
+});
