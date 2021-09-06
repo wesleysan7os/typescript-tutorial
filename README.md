f9b9131e95ca38a7d2dd212fd5fd87929da28d69
Silva <wesley.santos.silva@accenture.com>
2021-09-06 20:31:42 -0300

    arrays & objects

--- README.md
+++
@@ -1,4 +0,0 @@
-# typescript-tutorial
-Esse repositório possui todos os arquivos utilizados no curso 'TypeScript Tutorial' do canal The Net Ninja.
-# Como usar os arquivos
-Cada lição tem sua própria branch. Para ver o código referente a cada branch, selecione a lição no drop-down da branch.
--- sandbox.ts
+++ sandbox.ts
@@ -1,18 +1,37 @@
-let caracter = 'wes';
-let age = 30;
-let isBlackBelt = false;
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
 
-//caracter = 20;
-caracter = 'luigi';
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
 
-//age = 'yoshi';
-age = 40;
+let numbers = [10, 20, 12, 15];
 
-//isBlackBelt = 'yes';
-isBlackBelt = true;
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
-}
+let mixed = ['ken', 4, 'chun-li', 8, 9];
 
-console.log(circ(5.2));
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja = {
+  name: 'mario',
+  belt: 'black',
+  age: 30
+};
+
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};
