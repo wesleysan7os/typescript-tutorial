7ae648ebe7f58440db4a2b7729d1020f3f6e1ca5
Silva <wesley.santos.silva@accenture.com>
2021-09-26 20:04:14 -0300

    function basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,16 @@
 "use strict";
-console.log('test');
+var greet;
+//greet = 'hello';
+greet = function () {
+    console.log('hello, world!');
+};
+var add = function (a, b, c) {
+    if (c === void 0) { c = 10; }
+    console.log(a + b);
+    console.log(c);
+};
+add(3, 5, 1);
+var minus = function (a, b) {
+    return a - b;
+};
+var result = minus(4, 2);
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,22 @@
-console.log('test');
+let greet: Function;
+
+//greet = 'hello';
+
+greet = () => {
+  console.log('hello, world!');
+}
+
+const add = (a: number, b: number, c: number | string = 10) => {
+  console.log(a + b);
+  console.log(c);
+}
+
+add(3, 5, 1);
+
+const minus = (a: number, b: number): number => {
+  return a - b;
+}
+
+let result = minus(4, 2);
+
+
