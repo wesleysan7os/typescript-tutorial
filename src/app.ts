1c7988b28a6182b5448e0d45f332d4f941bc16df
Silva <wesley.santos.silva@accenture.com>
2021-09-28 23:18:54 -0300

    modules

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,4 @@
-// classes
-class Invoice {
-  //  readonly client: string;
-  //  private details: string;
-  //  public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number,
-  ) { }
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
@@ -27,7 +12,6 @@ invoices.forEach(inv => {
 });
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
