6d5d53cbb7e40a3b5edbe9c77e5717dcb6495b1b
Silva <wesley.santos.silva@accenture.com>
2021-09-28 23:35:26 -0300

    interfaces

--- public/app.js
+++ public/app.js
@@ -1,4 +1,19 @@
 import { Invoice } from './classes/Invoice.js';
+const me = {
+    name: 'wes',
+    age: 28,
+    speak(text) {
+        console.log(text);
+    },
+    spend(amount) {
+        console.log('I spent', amount);
+        return amount;
+    }
+};
+const greetPerson = (person) => {
+    console.log('hello ', person.name);
+};
+greetPerson(me);
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 let invoices = [];
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,31 @@
 import { Invoice } from './classes/Invoice.js';
 
+// interfaces
+interface IsPerson {
+  name: string;
+  age: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
+
+const me: IsPerson = {
+  name: 'wes',
+  age: 28,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent', amount);
+    return amount;
+  }
+};
+
+const greetPerson = (person: IsPerson) => {
+  console.log('hello ', person.name);
+}
+
+greetPerson(me);
+
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
 
