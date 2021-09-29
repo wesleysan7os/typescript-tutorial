2d88ac6291c28ba17b228ab69517dc6d279fbe82
Silva <wesley.santos.silva@accenture.com>
2021-09-28 22:45:44 -0300

    classes

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,35 @@
-// const anchor = document.querySelector('a')!;
+// classes
 
-// if(anchor) {
-//  console.log(anchor.href);
-// }
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
 
-// console.log(anchor.href);
-// const form = document.querySelector('form');
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes £${this.amount} for ${this.details}`;
+  }
+}
+
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'Wes';
+invTwo.amount = 666;
+
+console.log(invoices);
+
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-// console.log(form.children);
+
 
 // inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
